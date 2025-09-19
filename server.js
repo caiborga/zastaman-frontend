e0140a6fd7e847e8eb05b583a9ceb8f10cdab279
Stephan Kleinhans <4.da.crap@gmail.com>
2025-09-19 16:54:25 +0200

    changed server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+// server.js (CJS)
 const express = require("express");
 const path = require("path");
 const app = express();
 
-// statische Dateien aus /dist
-app.use(express.static(path.join(__dirname, "dist")));
+const dist = path.join(__dirname, "dist");
+app.use(express.static(dist, { etag: true, maxAge: "1y", index: false }));
 
-// SPA-Fallback
+app.get("/health", (_req, res) => res.send("ok"));
+
 app.get("*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(dist, "index.html"));
 });
 
 const port = process.env.PORT || 3000;
